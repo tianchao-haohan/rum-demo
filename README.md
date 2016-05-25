33151f0fbeea1687e5111ab9f14ea3892d87318a
hao han <hh.hao@outlook.com>
2016-05-24 19:01:21 -0700

    update files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# rum-demo
+curl -XPUT localhost:9200/_template/rum -d @apm_rum_template.json 
--- scripts/cmdb/virtualenv-12.1.1/ENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/intflow/workspace/python/cmdb/virtualenv-12.1.1/ENV/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
